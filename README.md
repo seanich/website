419786a1819133c1a78dc29d505a91b5a7af7fc6
Sean Nichols <git@seanich.com>
2019-09-07 21:54:57 -0400

    Fix PurgeCSS removing responsive classes

---
+++ README.md
@@ -0,0 +1,1 @@
+My personal website, [seanich.com](https://seanich.com).
--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,10 @@ module.exports = {
         require('tailwindcss'),
         require('autoprefixer'),
         ...(process.env.NODE_ENV === 'production'
-            ? [require('@fullhuman/postcss-purgecss')({ content: ['./src/**/*.html'] })]
+            ? [require('@fullhuman/postcss-purgecss')({
+                content: ['./src/**/*.html'],
+                defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+            })]
             : [])
     ]
 };
